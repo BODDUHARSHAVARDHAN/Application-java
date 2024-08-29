3d5e01abd6a99ac76c8c7f603d60cb3cb8bc99ba
BODDUHARSHAVARDHAN <109265742+BODDUHARSHAVARDHAN@users.noreply.github.com>
2024-08-29 16:24:10 +0530

    Create java-app.sh

---
+++ java-app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo -n tee /etc/systemd/system/java-app.service > /dev/null <<EOL
+[Unit]
+Description=Java Application Service
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/java -jar /opt/java-app/demo-0.0.1-SNAPSHOT.jar
+User=harsha
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo -n systemctl daemon-reload
+sudo -n systemctl enable java-app.service
+sudo -n systemctl start java-app.service
